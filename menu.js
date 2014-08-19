da2a31863e16fe16497ea357b9846ba551404cd4
FalconMasters <carlos@falconmasters.com>
2014-08-19 11:53:13 -0500

    Agregando Archivos

---
+++ menu.js
@@ -0,0 +1,22 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main (){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); Forma Sencilla de aparecer y desaparecer
+		
+		if (contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		};
+		
+	});
+};
